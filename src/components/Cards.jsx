6d07e4d96dce20ae93dd327fcc764255353f28dd
Byron Burnett <byron.burnett@outlook.com>
2024-02-22 17:01:08 +0200

    update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,8 @@ const Cards = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white ">
      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-     <Link to="/players"><div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
+     <Link to="/players">
+       <div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
        <img src={player} alt="player" />
        <h2 className="text-2xl font-bold text-center py-8">Players</h2>
        <p className="text-center font-medium">Check out our clients.</p>
