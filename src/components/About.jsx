ab24abb4270552582ef4c69707075ff9d4997edf
Byron Burnett <byron.burnett@outlook.com>
2024-03-11 16:30:29 +0200

    new contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,23 @@ const About = () => {
         <p className="text-[#07579f] font-bold text-4xl">Founder & CEO</p>
         <h1 className="md:text-4xl sm:text-3xl text-2xl font-medium py-2" >Byron Burnett</h1>
         <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2" >Certified FIBA Agent |License #202302530</h1>
-        <p className="">Hoopnomics was created through passion and determination that lends over 25 years of experience with the game of basketball. A former pro himself who had an extensive 14 year career as a professional, Byron played in 12 countries on 5 continents and intends to impart that knowledge and experience with the next crop of talented players in the game of basketball. <br/><br/> Mr. Burnett graduated from Florida International University with a bachelors in Liberal Arts in 2009 and later recieved a second degree in International Business from Metropolia University of Applied Sciences in Helsinki, Finland in 2023. Byron has gave back to the game as a writer, player, coach and referee throughout his illustrious career.</p>
-        <p></p>
+        <p className="">Hoopnomics was created through passion and determination that lends over 25 years of experience with the game of basketball. A former pro himself who had an extensive 14 year career as a professional, Byron played in 12 countries on 5 continents and intends to impart that knowledge and experience with the next crop of talented players in the game of basketball. <br/><br/> Mr. Burnett graduated from Florida International University with a bachelors in Liberal Arts in 2009 and later recieved a second degree in International Business from Metropolia University of Applied Sciences in Finland. Byron has worked in the game of basketball as a writer, player, coach, referee, and podcaster throughout his illustrious career.
+        <br/><br/>
+        What can you expect from me as an your Agent?<br/><br/>
+       <li>Stellar contract negotiation.</li>
+       <li>To market your talent and value as a player for desired results</li>
+       <li>To find the best fit for your talents</li>
+       <li>Knowledge of global basketball markets</li>
+       <br></br>
+       <p>
+        <a href="https://basketball.eurobasket.com/player/Byron-Burnett/32024">Link to Eurobasket Resume</a>
+       </p>
+       <p>
+        <a href="https://www.theseus.fi/handle/10024/809004" alt="Thesis">Thesis: The Impact of Data Analytics in Basketball.</a>
+       </p>
+       
+        </p>
+        
        </div>
       </div>
   </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,37 @@
 import React from 'react'
 
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+ 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_854rt9h', 'template_p5sxq3t', form.current, {
+        publicKey: 'UDDwXOqeq60d8Gfam',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          console.log('message sent');
+          e.target.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
+
+
+
+  
+  
   return (
     <div>
       <section className="text-gray-700 body-font relative">
@@ -14,32 +45,29 @@ const Contact = () => {
             </p>
           </div>
           <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <div className="flex flex-wrap -m-2">
+            <form ref={form}onSubmit={sendEmail} className="flex flex-wrap -m-2">
               <div className="p-2 w-1/2">
                 <div className="relative">
-                  <label for="name" className="leading-7 text-sm text-white">
+                  <label className="leading-7 text-sm text-white">
                     Name
                   </label>
                   <input
                     type="text"
                     id="name"
-                    name="name"
+                    name="user_name"
                     className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
               </div>
               <div className="p-2 w-1/2">
                 <div className="relative">
-                  <label
-                    for="email"
-                    className="leading-7 text-sm text-white"
-                  >
+                  <label  className="leading-7 text-sm text-white">
                     Email
                   </label>
                   <input
                     type="email"
                     id="email"
-                    name="email"
+                    name="user_email"
                     className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -61,11 +89,12 @@ const Contact = () => {
               </div>
               <div className="p-2 w-full text-center">
                 <button className="bg-[#07579f] text-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 ">
-                  Send
+                  
+                  <input type="submit" value="Send" />
                 </button>
               </div>
   
-            </div>
+            </form>
           </div>
         </div>
       </section>
