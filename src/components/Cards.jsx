31e851da819351c8d176dfa95313f3a324dfe896
Byron Burnett <byron.burnett@outlook.com>
2024-04-29 20:21:51 +0300

    fixed cards tab

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,14 +9,14 @@ const Cards = forwardRef((props, ref) => {
   return (
     <div ref={ref} className="w-full py-[10rem] px-4 bg-white ">
      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-     <Link to="/players">
+     
        <div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
        <img src={player} alt="player" />
        <h2 className="text-2xl font-bold text-center py-8">Players</h2>
        <p className="text-center font-medium">Coming Soon!</p>
        <button className="bg-[#07579f] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">Click Here</button>
       </div>
-     </Link>
+     
 
     
       <div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
