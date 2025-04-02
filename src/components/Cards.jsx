8782dbd8eb6491c9d81aeb15cb1d49919f5fd7b9
Byron Burnett <byron.burnett@outlook.com>
2025-04-02 13:31:29 +0300

    cards hero update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,35 +8,48 @@ import { Link } from 'react-router-dom/';
 const Cards = forwardRef((props, ref) => {
   return (
     <div ref={ref} className="w-full py-[10rem] px-4 bg-white ">
-     <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+     <div className="max-w-[960px] mx-auto grid md:grid-cols-2 gap-4">
      
-       <div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
-       <img src={player} alt="player" />
-       <h2 className="text-2xl font-bold text-center py-8">Players</h2>
-       <p className="text-center font-medium">Coming Soon!</p>
-      <div className="text-center">
-      <Link to="/"> <button  className="bg-blue-600 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">Click Here</button></Link> 
-      </div>
-      </div>
-     
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm hover:scale-105 duration-300">
+    <a href="#">
+        <img src={player} class="rounded-t-lg"  alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">Players</h5>
+        </a>
+        <p class="mb-3 font-normal text-black">Enter to see Hoopnomics players list, reach out if you are interested in signing any of our clients.</p>
+        <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
 
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm hover:scale-105 duration-300">
+    <a href="#">
+        <img src={Coaches} class="rounded-t-lg"  alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">Coaches</h5>
+        </a>
+        <p class="mb-3 font-normal text-black">Enter to see Hoopnomics coaches list, reach out if you are interested in signing any of our clients.</p>
+        <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
+
+
+
+     
     
-      <div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
-       <img src={Coaches} alt="coach" />
-       <h2 className="text-2xl font-bold text-center py-8">Coaches</h2>
-       <p className="text-center font-medium">Coming Soon!</p>
-       <button className="bg-blue-600 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">Click Here</button>
-      </div>
-     
-
-
-     
-      <div className="w-full border flex flex-col p-4 my-4 rounded-lg shadow-xl hover:scale-105 duration-300">
-       <img src={team1} alt="player" />
-       <h2 className="text-2xl font-bold text-center py-8">Teams</h2>
-       <p className="text-center font-medium">Coming Soon!</p>
-       <button className="bg-blue-600 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">Click Here</button>
-      </div>
    
       </div>
     </div>
