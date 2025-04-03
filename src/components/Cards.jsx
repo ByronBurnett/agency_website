3b4c92392e71d141cc623fea42c0663a06b2d714
Byron Burnett <byron.burnett@outlook.com>
2025-04-03 08:12:24 +0300

    cards update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {forwardRef} from 'react'
 import player from '../assets/players.jpg'
 import Coaches from '../assets/coach.png.jpg'
-import team1  from '../assets/team1.jpg'
+
 import { Link } from 'react-router-dom/';
 
 const Cards = forwardRef((props, ref) => {
@@ -10,39 +10,42 @@ const Cards = forwardRef((props, ref) => {
     <div ref={ref} className="w-full py-[10rem] px-4 bg-white ">
      <div className="max-w-[960px] mx-auto grid md:grid-cols-2 gap-4">
      
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm hover:scale-105 duration-300">
-    <a href="#">
-        <img src={player} class="rounded-t-lg"  alt="" />
-    </a>
+      <div className="max-w-sm bg-white border border-gray-400 rounded-lg shadow-lg hover:scale-105 duration-300">
+    
+        <img src={player} className="rounded-t-lg"  alt="player_pic" />
+    
     <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">Players</h5>
-        </a>
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">Players</h5>
+        
         <p class="mb-3 font-normal text-black">Enter to see Hoopnomics players list, reach out if you are interested in signing any of our clients.</p>
-        <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <Link to="/"> 
+        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </button>
+        </Link>
     </div>
 </div>
 
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm hover:scale-105 duration-300">
-    <a href="#">
-        <img src={Coaches} class="rounded-t-lg"  alt="" />
-    </a>
+<div className="max-w-sm bg-white border border-gray-400 rounded-lg shadow-lg hover:scale-105 duration-300">
+    
+        <img src={Coaches} className="rounded-t-lg"  alt="coach_pic" />
+    
     <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">Coaches</h5>
-        </a>
-        <p class="mb-3 font-normal text-black">Enter to see Hoopnomics coaches list, reach out if you are interested in signing any of our clients.</p>
-        <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+       
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">Coaches</h5>
+       
+        <p className="mb-3 font-normal text-black">Enter to see Hoopnomics coaches list, reach out if you are interested in signing any of our clients.</p>
+        <Link to="/"> 
+        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </button>
+        </Link>
     </div>
 </div>
 
