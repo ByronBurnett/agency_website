eff675b6bee5207f9224d700bbf7b25941bb2622
Byron Burnett <byron.burnett@outlook.com>
2025-04-24 23:29:32 +0300

    newer

--- src/components/PlayerDevelopment.jsx
+++ src/components/PlayerDevelopment.jsx
@@ -39,7 +39,7 @@ const PlayerDevelopment = () => {
        
       </div>
     </div>
-    <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700"></hr>
+    <hr class="w-48 h-1 mx-auto my-4 bg-black border-0 rounded-sm md:my-10 dark:bg-gray-700"></hr>
     </div>
      
 
