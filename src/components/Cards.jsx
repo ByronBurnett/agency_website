0911389d95d6985c5bf91d8b12c236f1c62da90e
Byron Burnett <byron.burnett@outlook.com>
2025-11-02 15:17:51 -0600

    new message

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,8 +17,7 @@ const Cards = forwardRef((props, ref) => {
             </h5>
 
             <p class="mb-3 font-normal text-black">
-              Enter to see Hoopnomics players list, reach out if you are
-              interested in signing any of our clients.
+              Enter our player portal to discover basketball talent backed by insight and data.
             </p>
             <Link to="">
               <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
