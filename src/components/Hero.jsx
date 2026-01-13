f37cc98708205a1b6ac6e2a4a2d8205f464e92ac
Byron Burnett <byron.burnett@outlook.com>
2026-01-13 16:24:45 -0600

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,11 @@ const Hero = () => {
        <p className="text-blue-600 text-4xl font-bold p-2">INSIDE THE NUMBERS</p>
        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Hoopnomics.</h1>
        <div className="flex justify-center items-center">
-<<<<<<< HEAD
         <p className="md:text-5xl sm:text-4xl font-bold py-4">We offer</p>
        <ReactTyped className="md:text-5xl sm:text-4xl font-bold md:pl-4 pl-2" strings={['individual', 'group',]} typeSpeed={120} backSpeed={140} loop />
        <p className="md:text-5xl sm:text-4xl font-bold py-4">sessions!</p>
-=======
         <p className="md:text-5xl sm:text-4xl font-bold py-4"></p>
        <ReactTyped className="md:text-5xl sm:text-4xl font-bold md:pl-4 pl-2 mb-5" strings={['Data Insight','&','Talent Discovery']} typeSpeed={120} backSpeed={140} loop />
->>>>>>> 0911389d95d6985c5bf91d8b12c236f1c62da90e
        </div>
        <p className="md:text-2xl text-xl font-bold text-gray-500">Creating value through player development</p>
        <Link to=""> 
